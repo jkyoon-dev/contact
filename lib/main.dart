bb5b9f0cc063f14d3ef6f5cfec0f784f0653fe3c
윤종경 <jkyoon@danawa.com>
2024-01-15 16:47:58 +0900

    Stateful위젯 및 setState 설정

--- lib/main.dart
+++ lib/main.dart
@@ -4,55 +4,55 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   var a = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Text(a.toString()),
-          onPressed: () {
-            print(a);
-            a++;
-          },
-        ),
         appBar: AppBar(
           title: Text('앱제목'),
         ),
         body: ShopItem(),
-        bottomNavigationBar: ButtomNavi(),
       ),
     );
   }
 }
 
-class ShopItem extends StatelessWidget {
+class ShopItem extends StatefulWidget {
   const ShopItem({super.key});
+
+  @override
+  State<ShopItem> createState() => _ShopItemState();
+}
+
+class _ShopItemState extends State<ShopItem> {
+  var name = ['김영숙', '홍길동', '피자집'];
+  var like = [0, 0, 0];
 
   @override
   build(BuildContext context) {
     return ListView.builder(
       itemCount: 3,
       itemBuilder: (context, index) {
-        return Text('홍길동 $index');
-      },
-    );
-  }
-}
-
-class ButtomNavi extends StatelessWidget {
-  const ButtomNavi({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 3,
-      itemBuilder: (context, index) {
         return ListTile(
-          leading: Image.asset('assets/star.jpg'),
-          title: Text('홍길동 $index'),
+          leading: Text("${name[index]} ${like[index]}"),
+          trailing: IconButton(
+            onPressed: () {
+              setState(() {
+                like[index]++;
+              });
+            },
+            icon: Icon(Icons.plus_one),
+          ),
         );
       },
     );
