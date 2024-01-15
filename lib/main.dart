e67691dc1a908ded5d03d9e93dfb0f1d126706c6
윤종경 <jkyoon@danawa.com>
2024-01-15 09:03:40 +0900

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +11,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Row(
+        appBar: AppBar(
+          title: Text('앱제목'),
+        ),
+        body: ShopItem(),
+        bottomNavigationBar: ButtomNavi(),
+      ),
+    );
+  }
+}
+
+// 변수랑 함수 담는 통 완성된 위젯 플래폼을 오른쪽에 ShopItem으로 복사
+class ShopItem extends StatelessWidget {
+  const ShopItem({super.key});
+
+  @override
+  build(BuildContext context) {
+    return ListView(
+      children: [
+        Row(
           children: [
-            Expanded(
-              child: Container(
-                color: Colors.amber,
-              ),
-            ),
-            Flexible(
-                child: Container(
-                  color: Colors.blue,
-                ),
-                flex: 7),
+            Icon(Icons.contact_page),
+            Text('홍길동'),
           ],
         ),
-      ),
+        Row(
+          children: [
+            Icon(Icons.contact_page),
+            Text('홍길동'),
+          ],
+        ),
+        Row(
+          children: [
+            Icon(Icons.contact_page),
+            Text('홍길동'),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ButtomNavi extends StatelessWidget {
+  const ButtomNavi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.call),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.message),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.contact_page_rounded),
+        ),
+      ],
     );
   }
 }
